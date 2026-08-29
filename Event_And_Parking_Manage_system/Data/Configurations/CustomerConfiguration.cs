b92af0f1c8fb7c662518410833e2caffb57819b7
Sujeepan <psujee07@gmailcom>
2026-08-29 23:50:45 +0530

    Member 1 model created, and EF core Setup

---
+++ Event_And_Parking_Manage_system/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Event_And_Parking_Manage_system.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Event_And_Parking_Manage_system.Data.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(x => x.CustomerId);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
+            builder.HasIndex(x => x.Email).IsUnique();
+            builder.Property(x => x.Phone).IsRequired().HasMaxLength(20);
+            builder.Property(x => x.PasswordHash).IsRequired().HasMaxLength(500);
+            builder.Property(x => x.EmailVerificationTokenHash).HasMaxLength(500);
+            builder.Property(x => x.PasswordResetTokenHash).HasMaxLength(500);
+        }
+    }
+}
---
+++ Event_And_Parking_Manage_system/Models/Enums/CustomerStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Event_And_Parking_Manage_system.Models.Enums
+{
+    public enum CustomerStatus
+    {
+        Active = 1,
+        Deactivated = 2
+    }
+}
