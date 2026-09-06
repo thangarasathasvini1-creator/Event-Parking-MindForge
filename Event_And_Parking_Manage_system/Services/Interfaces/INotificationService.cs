b38226bd232fba03808ca340e61fed8c6fd27b59
Sujeepan <sujeepansujee07@gmail.com>
2026-09-06 07:53:47 +0530

    Complete Member 1 backend tasks

---
+++ Event_And_Parking_Manage_system/Services/Interfaces/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using Event_And_Parking_Manage_system.DTOs.Notifications;
+
+namespace Event_And_Parking_Manage_system.Services.Interfaces
+{
+    public interface INotificationService
+    {
+        Task<List<NotificationDto>> GetCustomerNotificationsAsync(
+            int customerId);
+
+        Task<bool> MarkAsReadAsync(
+            int notificationId,
+            int customerId);
+    }
+}
