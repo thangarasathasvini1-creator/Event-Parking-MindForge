f80e0d03f6b32a98219c8236c24b9c04c2080d76
Sujeepan <sujeepansujee07@gmail.com>
2026-09-11 11:09:54 +0530

    complete customer dashboard and profile

---
+++ event-parking-angular/src/app/features/auth/pages/login-callback/login-callback.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginCallback } from './login-callback';
+
+describe('LoginCallback', () => {
+  let component: LoginCallback;
+  let fixture: ComponentFixture<LoginCallback>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoginCallback],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoginCallback);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ event-parking-angular/src/app/models/dashboard.model.ts
@@ -0,0 +1,6 @@
+export interface Dashboard {
+  upcomingBookings: number;
+  reservedParking: number;
+  recentPayments: number;
+  unreadNotifications: number;
+}
