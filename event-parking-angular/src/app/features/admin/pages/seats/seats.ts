c00354f69c094f24bd164099b8fe0287b0b693a8
jochimthilaharaj2001 <bthilaharaj@gmail.com>
2026-09-12 11:51:11 +0530

    member 3 seat reservation

---
+++ event-parking-angular/src/app/features/admin/pages/seats/seats.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seats',
+  imports: [],
+  templateUrl: './seats.html',
+  styleUrl: './seats.css',
+})
+export class Seats {}
---
+++ event-parking-angular/src/app/shared/components/parking-slot-button/parking-slot-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-slot-button',
+  imports: [],
+  templateUrl: './parking-slot-button.html',
+  styleUrl: './parking-slot-button.css',
+})
+export class ParkingSlotButton {}
